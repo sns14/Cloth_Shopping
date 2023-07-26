aa4342ff6ec9391f11135073913027db4db5c345
Sucheth N S <108053605+sns14@users.noreply.github.com>
2023-07-26 10:41:00 +0530

    Add files via upload
    Initial commit

---
+++ data.js
@@ -0,0 +1,51 @@
+let shopItemsData=[
+    {
+    id:"id1",
+    name:"PELUCHE",
+    price: 1250,
+    desc: "Grey light Bow Tie",
+    img: "images/img1.jpg"
+},
+{
+    id:"id2",
+    name:"Van Heusen",
+    price: 1000,
+    desc: "Men Pure Cotton Opaque Formal Shirt",
+    img: "images/img2.jpg"
+},
+{
+    id:"id3",
+    name:"Monte Carlo",
+    price:3000,
+    desc: "Men brown Cable Knit V Neck Pullover",
+    img: "images/img3.jpg"
+},
+{
+    id:"id4",
+    name:"DeFacto",
+    price: 3120,
+    desc: "Men Regular Fit Mid Rise dark Jeans",
+    img: "images/img4.jpg"
+},
+{
+    id:"id5",
+    name:"Peter England Elite",
+    price: 12000,
+    desc: "Men Blue Solid Slim-Fit 3-Piece Formal Suit",
+    img: "images/img5.jpg"
+},
+{
+    id:"id6",
+    name:"Puma",
+    price: 1600,
+    desc: "Men light Blue Essential Small Logo Hooded Sweatshirt",
+    img: "images/img6.jpg"
+},
+{
+    id:"id7",
+    name:"U.S. Polo Assn",
+    price: 980,
+    desc: "Men Polo Collar Pure Cotton Slim Fit T-shirt",
+    img: "images/img7.jpg"
+},
+];
---
+++ main.js
@@ -0,0 +1,80 @@
+let shop=document.getElementById('shop')
+
+let basket=JSON.parse(localStorage.getItem("data")) || [];//to implement data storage in cart symbol
+
+let generateshop=()=>{
+    return (shop.innerHTML=shopItemsData.map((x)=>{
+        let {id,name,price,desc,img}=x
+        let search=basket.find((x)=>x.id===id) || [];
+        return `
+        <div id=product-id-${id} class="item">
+                <img width="220"src=${img} alt="">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-quantity">
+                        <h2>₹ ${price}</h2>
+                        <div class="buttons">
+                            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                            <div id=${id} class="quantity">
+                            ${search.item === undefined? 0: search.item}
+                            </div>
+                            <i onclick="increment(${id})"class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>` ;
+    }).join(""));
+};
+
+generateshop();
+
+let increment = (id)=>{
+    let selectedItem =id;
+    let search=basket.find((x)=> x.id === selectedItem.id);
+
+    if(search === undefined){
+        basket.push({
+            id:selectedItem.id,
+            item:1,
+        });  
+    }
+
+    else{
+        search.item +=1;
+    }
+    //console.log(basket);
+    update(selectedItem.id);//increment in the actual box
+    localStorage.setItem("data",JSON.stringify(basket));
+};
+
+let decrement = (id)=>{
+    let selectedItem =id;
+    let search=basket.find((x)=> x.id === selectedItem.id);
+    if(search===undefined) return;
+    else if(search.item === 0) return;
+
+    else{
+        search.item -=1;
+    }
+    update(selectedItem.id);//decrement in the actual box
+
+    basket=basket.filter((x)=>x.item!=0);
+    //console.log(basket);
+    
+    localStorage.setItem("data",JSON.stringify(basket));
+};
+let update = (id)=>{
+    let search= basket.find((x)=>x.id===id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML=search.item;//increment and decrement display
+    calculation();
+};
+
+let calculation=()=>{
+    let cartIcon=document.getElementById("cartamount");
+    cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);//upper cart icon increment and decrement
+    // console.log(basket.map((x)=>x.item).reduce((x,y)=>x+y,0));
+};
+
+calculation();//even after refresh upper cartval remain const
